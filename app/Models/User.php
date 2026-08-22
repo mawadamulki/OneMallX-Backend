0941e763e4307e603bbc119a4a9576735184ecf5
mawadamulki <r.memomulki555@gmail.com>
2026-08-22 12:21:26 +0300

    delete adding permission table

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
-    
+
 
     /**
      * The attributes that are mass assignable.
